5fd55917e3078947cc27bdedeef683b7a2866225
Julien Leveneur <73287307+J-Leveneur@users.noreply.github.com>
2023-07-04 12:44:28 +0000

    Update Alert class

--- alert.ts
+++ alert.ts
@@ -1,21 +1,29 @@
 import Catastrophe from "./catastrophe";
+import Preference from "./preference";
+import Place from "./place";
 
 class Alert {
   private start: Date;
   private end?: Date;
   private description?: string;
   private catastrophe: Catastrophe;
+  private preference: Preference;
+  private place: Place;
 
   constructor(
     start: Date,
     end: Date,
     description: string,
-    catastrophe: Catastrophe
+    catastrophe: Catastrophe,
+    Preference: Preference,
+    place: Place
   ) {
     this.start = start;
     this.end = end;
     this.description = description;
     this.catastrophe = catastrophe;
+    this.preference = Preference;
+    this.place = place;
   }
 
   public getStart(): Date {
@@ -32,6 +40,14 @@ class Alert {
 
   public getCatastrophe(): Catastrophe {
     return this.catastrophe;
+  }
+
+  public getPreference(): Preference {
+    return this.preference;
+  }
+
+  public getPlace(): Place {
+    return this.place;
   }
 }
 
